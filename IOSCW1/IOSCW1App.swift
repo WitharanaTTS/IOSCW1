ca5c100211ca8e0c3c3dc88a651413df327a0d5f
ThushaniSankalpana <witharanathushani.sankalpana@gmail.com>
2023-09-23 20:09:07 +0530

    Initial Commit

---
+++ IOSCW1/IOSCW1App.swift
@@ -0,0 +1,17 @@
+//
+//  IOSCW1App.swift
+//  IOSCW1
+//
+//  Created by Lakna Attigala on 2023-09-23.
+//
+
+import SwiftUI
+
+@main
+struct IOSCW1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
